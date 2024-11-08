05d957349dbf06193d818c8cee8b9e75e2ebb38c
Remy-Rapicault <remy.rapicault.dwwm@gmail.com>
2024-11-08 14:26:14 +0100

    apiUrl ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const tokenCookieName = "accesstoken";
 const roleCookieName = "role";
 const signoutBtn = document.getElementById("signout-btn");
-const apiUrl = "http://127.0.0.1:8000/api/"; // <= Url site d'hébergement
+const apiUrl = "http://127.0.0.1:8000/api/"; 
 
 signoutBtn.addEventListener("click", signout);
 
